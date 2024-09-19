7831b0e3c26b6e66e2bb5293d035d28fb175bde9
Ivan <60666284+ivanmp3@users.noreply.github.com>
2024-09-19 09:11:56 +0300

    Create JavaArrayListExercise.java

---
+++ JavaArrayListExercise.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone {
+  public static void main(String[] args) throws java.lang.Exception {
+    ArrayList < String > colors = new ArrayList < String > ();
+    colors.add("red");
+    colors.add("green");
+    colors.add("grey");
+    colors.add("purple");
+    for (int i = 0; i < colors.size(); i++) {
+      System.out.println(colors.get(i));
+    }
+    colors.add(0, "pink");
+    colors.get(2);
+    colors.set(3, "yellow");
+    colors.remove(3);
+    boolean areBlackPresent = colors.contains("black");
+    Collections.sort(colors);
+    ArrayList < String > sortedColors = new ArrayList < String > ();
+    sortedColors = (ArrayList) colors.clone();
+    Collections.reverse(colors);
+    boolean areEqual = colors.equals(sortedColors);
+    int size = colors.size();
+    colors.clear();
+    size = colors.size();
+    System.out.println(size);
+    colors.ensureCapacity(50);
+    sortedColors.trimToSize();
+  }
+}
