93ed6eb8b951ebc5a03d2b3127267b04a53eb874
Ivan <60666284+ivanmp3@users.noreply.github.com>
2024-10-08 08:28:54 +0300

    add final to Solution.java

--- LinkedList/Solution.java
+++ LinkedList/Solution.java
@@ -1,22 +1,21 @@
 package LinkedList;
 
-
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+final class Solution {
+    public final ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode listR = new ListNode(0);
-        ListNode result = listR;
+        final ListNode result = listR;
 
         while (list1 != null && list2 != null) {
             if (list1.val <= list2.val) {
                 listR.next = new ListNode(list1.val);
-                list1 = list1.next;
+                list1 = list1.next; 
             } else {
                 listR.next = new ListNode(list2.val);
-                list2 = list2.next;
+                list2 = list2.next;  
             }
-            listR = listR.next;
+            listR = listR.next;  
         }
-
+        
         if (list1 != null) {
             listR.next = list1;
         } else if (list2 != null) {
