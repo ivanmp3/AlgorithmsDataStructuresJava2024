3cf3a06f5a6931a8b33ec5c234ac463c2b0539dd
Ivan <60666284+ivanmp3@users.noreply.github.com>
2024-10-08 08:40:09 +0300

    removed printList from Main.java

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -5,37 +5,21 @@ public class Main {
         Solution solution = new Solution();
 
         // Example 1
-        LinkedList.ListNode list1_1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-        LinkedList.ListNode list2_1 = new ListNode(1, new ListNode(3, new ListNode(4)));
-        LinkedList.ListNode merged1 = solution.mergeTwoLists(list1_1, list2_1);
-        printList(merged1); // Ожидаемый вывод: 1 -> 1 -> 2 -> 3 -> 4 -> 4
+        ListNode list1_1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2_1 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode merged1 = solution.mergeTwoLists(list1_1, list2_1);
+        System.out.println(merged1); // Ожидаемый вывод: 1 -> 1 -> 2 -> 3 -> 4 -> 4
 
         // Example 2
         ListNode list1_2 = null;
         ListNode list2_2 = null;
         ListNode merged2 = solution.mergeTwoLists(list1_2, list2_2);
-        printList(merged2); // Ожидаемый вывод: пустой список
+        System.out.println(merged2 != null ? merged2 : "Список пустой"); // Ожидаемый вывод: пустой список
 
         // Example 3
         ListNode list1_3 = null;
         ListNode list2_3 = new ListNode(0);
         ListNode merged3 = solution.mergeTwoLists(list1_3, list2_3);
-        printList(merged3); // Ожидаемый вывод: 0
-    }
-
-    // Метод для вывода связного списка
-    public static void printList(ListNode head) {
-        if (head == null) {
-            System.out.println("Список пустой");
-            return;
-        }
-        while (head != null) {
-            System.out.print(head.val);
-            if (head.next != null) {
-                System.out.print(" -> ");
-            }
-            head = head.next;
-        }
-        System.out.println();
+        System.out.println(merged3); // Ожидаемый вывод: 0
     }
 }
